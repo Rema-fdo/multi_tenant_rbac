0f33aa15b8a1a44ab45b0441f37fddffa8f8e9fc
Brinton Fernando G <remafdo181299@gmail.com>
2025-03-26 12:25:29 +0530

    added routes to the server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
-
+const routes = require("./routes/routes.js");
 dotenv.config();
 const sequelize = require("./config/database");
 
 const app = express();
 app.use(bodyParser.json());
+app.use("/", routes);
 
 const PORT = process.env.PORT || 5000;
 
