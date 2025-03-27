dcdd84f94d56a0954b31a03b9828542615012faa
Brinton Fernando G <remafdo181299@gmail.com>
2025-03-27 11:37:55 +0530

    modified routes for superAdmin

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const authRoutes = require("./auth.routes");
 const userRoutes = require("./user.routes")
 const tenantRoutes = require("./tenant.routes")
+const superAdminRoutes = require("./superAdmin.routes")
 
 router.use("/auth", authRoutes);
 router.use("/", userRoutes)
 router.use("/", tenantRoutes)
+router.use("/", superAdminRoutes)
 
 module.exports = router;
