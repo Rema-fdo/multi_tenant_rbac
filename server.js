101f3a46e3be3e2e3f0136a1cf72d8f2ee10f560
Brinton Fernando G <remafdo181299@gmail.com>
2025-03-29 12:09:47 +0530

    added swagger configuration

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const routes = require("./routes/routes.js");
+const setupSwagger = require("./config/swagger");
+
 dotenv.config();
 const sequelize = require("./config/database");
 
@@ -9,10 +11,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use("/", routes);
 
+setupSwagger(app);
+
 const PORT = process.env.PORT || 5000;
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
   });
 });
